cd918a9ca222b9404900059e64babe04eafcf61a
Peer Naupold <greenion@gmx.de>
2020-05-08 23:44:21 +0200

    added search functionality and implemented stripe API

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'accounts',
     'products',
     'cart',
+    'search',
 ]
 
 MIDDLEWARE = [
@@ -134,9 +135,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    )
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
+STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
 """
 #workaround for issue in C9 with messages 
--- env.py
+++ env.py
@@ -2,3 +2,6 @@ import os
 
 os.environ.setdefault("SECRET_KEY", "mq(6ctmt$1v779m^k=5vw_&g=yj#bypo5yo3$g-uv$ix1bi_3u")
 os.environ.setdefault('LOCAL_HOSTNAME', '127.0.0.1')
+
+os.environ.setdefault('STRIPE_PUBLISHABLE', 'pk_test_6WSv3MVn0W5eZOTUMumkYQzq0078SedWxY')
+os.environ.setdefault('STRIPE_SECRET', 'sk_test_auJI3KaDMnrerqV0jBADIOEU0064hbP3qD')
