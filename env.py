395639fef5e836d264d58d3a4dc14c832195db9e
Peer Naupold <greenion@gmx.de>
2020-05-16 00:04:50 +0200

    added S3 storage

--- env.py
+++ env.py
@@ -6,3 +6,6 @@ os.environ.setdefault('LOCAL_HOSTNAME', '127.0.0.1')
 os.environ.setdefault('STRIPE_PUBLISHABLE', 'pk_test_6WSv3MVn0W5eZOTUMumkYQzq0078SedWxY')
 os.environ.setdefault('STRIPE_SECRET', 'sk_test_auJI3KaDMnrerqV0jBADIOEU0064hbP3qD')
 os.environ.setdefault('DATABASE_URL', 'postgres://hdljqenvdrhada:7d365992752855872e57f52f4f41992ecdb1400203a73cdce771f23fd077f92d@ec2-54-247-89-181.eu-west-1.compute.amazonaws.com:5432/d77io58jfs90p0')
+
+os.environ.setdefault('AWS_ACCESS_KEY_ID','AKIAUHEQRDBBPQV557O2')
+os.environ.setdefault('AWS_SECRET_ACCESS_KEY','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')
