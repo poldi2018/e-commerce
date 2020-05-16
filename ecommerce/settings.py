4fa7e50e7cb8af5ffa099bdaec1c2a96031e8516
Peer Naupold <greenion@gmx.de>
2020-05-16 03:16:39 +0200

    updated settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -90,12 +90,19 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 
-if "DATABASE_URL" in os.environ:
-    DATABASES = {'default': dj_database_url.parse(os.getenv("DATABASE_URL"))}
+# if "DATABASE_URL" in os.environ:
+#     DATABASES = {'default': dj_database_url.parse(os.getenv("DATABASE_URL"))}
 
-else:
-    print("Database URL not found. Using SQLite instead")
-    DATABASES = {
+# else:
+#     print("Database URL not found. Using SQLite instead")
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
+
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
