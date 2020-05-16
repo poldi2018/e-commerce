e6de5e75a33acf5305e2cf3639fe1fb729638d3a
Peer Naupold <greenion@gmx.de>
2020-05-16 04:15:13 +0200

    removed postgres for testing

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -106,12 +106,12 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 
 print("Database URL not found. Using SQLite instead")
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
 
 
 # Password validation
