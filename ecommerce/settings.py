1c251c59717438f0e1fd8c8d4867d689589b5274
Peer Naupold <greenion@gmx.de>
2020-05-16 03:12:32 +0200

    updated settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -99,8 +99,8 @@ else:
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
 
 
 # Password validation
